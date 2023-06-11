2be0866fbf5846a75d84fb9553ccb98b3567e345
yurim <nogod829@gmail.com>
2023-06-11 15:44:04 +0900

    해더 제이쿼리 수정

--- js/main.js
+++ js/main.js
@@ -7,10 +7,15 @@ $('.gnb>li').hover(function (e) {
     $(this).find('.sub').show()
 }, function (e) {
     e.preventDefault();
-    $('header').css({"background-color":"","border-bottom":""});
-    $('.logo a').css('filter', '');
-    $('.gnb').css("color","");
-    $('.member a').css("filter","");
+    scroll = $(window).scrollTop();
+    ofTop = $('.pick_wrap').offset().top - 60;
+    if (scroll >= ofTop) {
+        $('.header').addClass('on');
+        $('header').css({"background-color":"#fff","border-bottom":"solid 1px #eaeaea"});
+        $('.logo a').css('filter', 'inherit');
+        $('.gnb').css("color","#1A1A1A");
+        $('.member a').css("filter","inherit");
+    } else { }
     $('.gnb>li').find('.sub').hide()
 });
 function menu() {
