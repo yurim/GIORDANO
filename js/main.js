103e99954f7a26fed66647048e0fc0c018f44b4c
yurim <nogod829@gmail.com>
2023-06-11 15:34:14 +0900

    해더수정

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
-$('.gnb>li').hover(function () {
+$('.gnb>li').hover(function (e) {
+    e.preventDefault();
     $('header').css({"background-color":"#fff","border-bottom":"solid 1px #eaeaea"});
     $('.logo a').css('filter', 'inherit');
     $('.gnb').css("color","#1A1A1A");
     $('.member a').css("filter","inherit");
     $(this).find('.sub').show()
-}, function () {
+}, function (e) {
+    e.preventDefault();
     $('header').css({"background-color":"","border-bottom":""});
     $('.logo a').css('filter', '');
     $('.gnb').css("color","");
     $('.member a').css("filter","");
     $('.gnb>li').find('.sub').hide()
 });
+function menu() {
+    scroll = $(window).scrollTop();
 
-const Header = document.querySelector('header');
-document.addEventListener('scroll', function(e){
-    e.preventDefault();
-    let scroll = window.scrollY
-    if(scroll > 50) {
-        Header.classList.add('on');
-        Header.style.background = "#fff";
-        Header.style.borderBottom = '1px solid #eaeaea';
+    ofTop = $('.pick_wrap').offset().top - 60;
+    if (scroll >= ofTop) {
+        $('.header').addClass('on');
+        $('header').css({"background-color":"#fff","border-bottom":"solid 1px #eaeaea"});
+        $('.logo a').css('filter', 'inherit');
+        $('.gnb').css("color","#1A1A1A");
+        $('.member a').css("filter","inherit");
     } else {
-        Header.classList.remove('on');
-        Header.style.background = null;
-        Header.style.borderBottom = null;
+        $('header').css({"background-color":"","border-bottom":""});
+        $('.logo a').css('filter', '');
+        $('.gnb').css("color","");
+        $('.member a').css("filter","");
     }
-});
-
+}
+menu();
+$(window).scroll(function () {
+    menu()
+})
